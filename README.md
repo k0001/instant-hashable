18b3b742ac360ae9c1582c411127246085399e93
Renzo Carbonara <gnuk0001@gmail.com>
2015-07-14 17:26:00 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+`instant-hashable` allows you to generically derive `Data.Hashable.Hashable` instances
+(from the `hashable` package) for your types using `instant-generics`,
+which in particular, supports deriving generic representations for GADTs.
---
+++ src/lib/Generics/Instant/Functions/Hashable.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+
+module Generics.Instant.Functions.Hashable
+  ( -- $defaults
+    ghashWithSalt
+    -- * Internals
+  , GHashable
+  ) where
+
+import           Data.Hashable (Hashable(hashWithSalt))
+import           Generics.Instant
+
+--------------------------------------------------------------------------------
+-- $defaults
+--
+-- You can use 'ghashWithSalt' as your generic 'hashWithSalt' for any
+-- 'Representable' type as follows:
+--
+-- @
+-- instance 'Hashable' MyType where hashWithSalt = 'ghashWithSalt'
+-- @
+
+ghashWithSalt :: (Representable a, GHashable (Rep a)) => Int -> a -> Int
+ghashWithSalt = \s a -> ghashWithSalt' s (from a)
+{-# INLINABLE ghashWithSalt #-}
+
+--------------------------------------------------------------------------------
+
+class GHashable a where
+  ghashWithSalt' :: Int -> a -> Int
+
+instance GHashable Z where
+  ghashWithSalt' _ _ = error
+    "Generics.Instant.Functions.DeepSeq.GHashable Z ghashWithSalt' - impossible"
+
+instance GHashable U where
+  ghashWithSalt' s U = hashWithSalt s ()
+  {-# INLINABLE ghashWithSalt' #-}
+
+instance GHashable a => GHashable (CEq c p q a) where
+  ghashWithSalt' s (C a) = ghashWithSalt' s a
+  {-# INLINABLE ghashWithSalt' #-}
+
+instance (GHashable a, GHashable b) => GHashable (a :*: b) where
+  ghashWithSalt' s (a :*: b) = ghashWithSalt' (ghashWithSalt' s a) b
+  {-# INLINABLE ghashWithSalt' #-}
+
+instance (GHashable a, GHashable b) => GHashable (a :+: b) where
+  ghashWithSalt' s lr = 0 `hashWithSalt` case lr of
+    L a -> Left  (ghashWithSalt' s a)
+    R b -> Right (ghashWithSalt' s b)
+  {-# INLINABLE ghashWithSalt' #-}
+
+instance Hashable a => GHashable (Var a) where
+  ghashWithSalt' s (Var a) = hashWithSalt s a
+  {-# INLINABLE ghashWithSalt' #-}
+
+instance Hashable a => GHashable (Rec a) where
+  ghashWithSalt' s (Rec a) = hashWithSalt s a
+  {-# INLINABLE ghashWithSalt' #-}
