83aa01305a0efb48c828c8bc20cbf2479f638f4f
Renzo Carbonara <gnuk0001@gmail.com>
2015-08-08 20:12:59 -0300

    add nix files

---
+++ nix/default.nix
@@ -0,0 +1,23 @@
+{ mkDerivation
+, stdenv
+
+  # haskell deps
+, base
+, hashable
+, instant-generics
+, tasty
+, tasty-quickcheck
+}:
+
+mkDerivation {
+  pname = "instant-hashable";
+  version = "0.1.0.1";
+  homepage = "https://github.com/k0001/instant-hashable";
+  description = "Generic Hashable instances through instant-generics";
+  license = stdenv.lib.licenses.bsd3;
+  src = ../.;
+  isLibrary = true;
+  isExecutable = false;
+  doHaddock = true;
+  buildDepends = [base hashable instant-generics tasty tasty-quickcheck];
+}
